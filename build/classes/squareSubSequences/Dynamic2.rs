56ce7e6d4825842c94012592a66cb0e469ab885c
aimjy <jaime@macbook-pro-2.home>
2017-03-25 22:10:19 +0100

    Project algortime & datastructuren

---
+++ build/classes/squareSubSequences/Dynamic2.rs
@@ -0,0 +1,1 @@
+squareSubsequences.Dynamic2
---
+++ src/squareSubSequences/Dynamic1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package squareSubsequences;
+
+/**
+ *
+ * @author Leen De Baets
+ */
+public class Dynamic1 {
+
+    public Dynamic1() {
+    }
+        
+    
+    /**
+     * You can use the class Square Subsequence 
+     * 
+     * @param s a string   
+     * @return The amount of square subsequences in string s is returned. 
+     */
+    public static int amountOfSquareSubSequences(String s){
+        if (true){
+            throw new UnsupportedOperationException("Implement amountOfSquareSubSequences in class Dynamic1.");
+        }
+        
+        return 0;
+    }
+}
